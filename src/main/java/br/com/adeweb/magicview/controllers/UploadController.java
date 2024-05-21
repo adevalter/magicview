800f078e71c420afede99abca060918f43742d70
Adevalter <adevalter@hotmail.com>
2024-05-21 13:28:28 -0300

    alterado controller e retorno image

--- src/main/java/br/com/adeweb/magicview/controllers/UploadController.java
+++ src/main/java/br/com/adeweb/magicview/controllers/UploadController.java
@@ -1,5 +1,6 @@
 package br.com.adeweb.magicview.controllers;
 
+import br.com.adeweb.magicview.models.Upload;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,10 +25,10 @@ public class UploadController {
   public ResponseEntity<Object> uploadFile(@RequestParam("file") MultipartFile file) {
     String message = "";
     try {
-      service.save(file);
+    Upload fileSave = service.save(file);
 
       message = "Uploaded the file successfully: " + file.getOriginalFilename();
-      return ResponseEntity.status(HttpStatus.OK).body(message);
+      return ResponseEntity.status(HttpStatus.OK).body(fileSave.getId());
     } catch (Exception e) {
       message = "Could not upload the file: " + file.getOriginalFilename() + ". Error: " + e.getMessage();
       return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(message);
